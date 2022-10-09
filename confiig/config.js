0e578eca5b4b67dd8c35bcea418dc77b5d660109
Amir Rahi <amirrahi29@gmail.com>
2022-10-09 22:08:31 +0530

    my note app node js apis

--- confiig/config.js
+++ confiig/config.js
@@ -2,8 +2,8 @@ const config = {
 
     PORT:9000,
     SECURE_JWT_TOKEN: "jskdgfisudgvisudv8787y7dsvy7s8d",
-    admin_email:"yourcompany2008@gmail.com",
-    admin_password:"yfummldlnhlllriv"
+    admin_email:"",
+    admin_password:""
 
 }
 
